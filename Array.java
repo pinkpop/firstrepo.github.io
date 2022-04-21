c6ef0d334d894e48764000c1eb69d954399eb600
pinkpop <1469137335@qq.com>
2022-04-21 14:22:27 +0900

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,42 @@
+package com.codewithmosh;
+
+public class Array {
+    private int[] items;
+    private int count;
+
+    public Array(int length){
+      items = new int[length];
+    }
+    public void insert(int number){
+        if(items.length == count){
+            int[] newItems= new int[count * 2];
+            for(int i = 0; i < count; i++){
+                newItems[i] = items[i];
+            }
+            items = newItems;
+        }
+        items[count++] = number;
+    }
+    public void removeAt(int index){
+        if(index < 0 || index >= count){
+            throw new IllegalArgumentException();
+        }
+        for ( int i = index; i < count; i++){
+            items[i] = items[i+1];
+            count--;
+        }
+    }
+
+    public int indexOf(int item){
+        for(int i = 0; i < count; i++){
+            if(item == items[i]){
+                return i;
+            }
+        } return -1;
+    }
+    public void print(){
+        for( int i = 0; i < count; i++){
+            System.out.println(items[i]);
+        }
+    }
+}
