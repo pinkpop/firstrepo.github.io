a1574a7a03836a569d5b8f4ffb646b82c3054477
pinkpop <1469137335@qq.com>
2022-07-01 11:32:53 +0900

    Time: 2 ms (99.96%), Space: 42.5 MB (85.39%) - LeetHub

---
+++ 654-maximum-binary-tree/654-maximum-binary-tree.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+            return build(nums, 0, nums.length - 1);
+    }
+
+    /* 定义：将 nums[lo..hi] 构造成符合条件的树，返回根节点 */
+    TreeNode build(int[] nums, int lo, int hi) {
+        // base case
+        if (lo > hi) {
+            return null;
+        }
+
+        // 找到数组中的最大值和对应的索引
+        int index = -1, maxVal = Integer.MIN_VALUE;
+        for (int i = lo; i <= hi; i++) {
+            if (maxVal < nums[i]) {
+                index = i;
+                maxVal = nums[i];
+            }
+        }
+
+        TreeNode root = new TreeNode(maxVal);
+        // 递归调用构造左右子树
+        root.left = build(nums, lo, index - 1);
+        root.right = build(nums, index + 1, hi);
+
+        return root;
+    }
+}
