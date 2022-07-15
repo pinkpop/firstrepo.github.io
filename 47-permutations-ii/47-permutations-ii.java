f5529ece2d5b75da648c3473baf78e2568540722
pinkpop <1469137335@qq.com>
2022-07-15 23:06:13 +0900

    Time: 8 ms (37.66%), Space: 49.1 MB (31.95%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.java
@@ -0,0 +1,32 @@
+class Solution {
+    List<List<Integer>> res = new LinkedList<>();
+    LinkedList<Integer> track = new LinkedList<>();
+    boolean[] used;
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        used = new boolean[nums.length];
+        backtrack(nums);
+        return res;
+    }
+    
+    private void backtrack(int[] nums){
+        if(track.size() == nums.length){
+            res.add(new LinkedList<Integer>(track));
+            return;
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            if(used[i]){
+                continue;
+            }
+            if(i > 0 && nums[i] == nums[i - 1] && !used[i-1]){
+                continue;
+            }
+            track.addLast(nums[i]);
+            used[i] = true;
+            backtrack(nums);
+            track.removeLast();
+            used[i] = false;
+        }
+    }
+}
