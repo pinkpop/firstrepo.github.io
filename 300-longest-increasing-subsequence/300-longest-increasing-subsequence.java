006993b503213ff14f89b5d6faf925aeafed36b1
pinkpop <1469137335@qq.com>
2022-10-03 23:00:57 +0900

    Time: 93 ms (32.00%), Space: 44.9 MB (26.34%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        //base case dp[i] = 1
+        Arrays.fill(dp,1);
+        
+        for(int i = 0; i < nums.length; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int res = 0;
+        //答案为dp数组里的最大值
+        for(int i = 0; i < dp.length; i++){
+            res = Math.max(res, dp[i]);
+        }
+        return res;
+    }
+ 
+}
